00594c47b70a8ab5cdd219d07c64f06ac1f94df0
John-Paul Chirwa <johnpaulchirwa@live.co.uk>
2018-08-29 01:20:25 +0100

    Day 4

--- NyasaConnect/www/js/my-app.js
+++ NyasaConnect/www/js/my-app.js
@@ -1,46 +1,23 @@
 // Initialize app
-var myApp = new Framework7();
-
+var myApp = new Framework7({
+    pushState: true, //Android back button go back a page
+    init: false //Disable App's automatic initialization
+});
+// Option 1. Using page callback for page (for "about" page in this case) (recommended way):
+myApp.onPageInit('index', function (page) {
+    myApp.alert("Welcome");
+})
 
 // If we need to use custom DOM library, let's save it to $$ variable:
 var $$ = Dom7;
 
 // Add view
 var mainView = myApp.addView('.view-main', {
-    // Because we want to use dynamic navbar, we need to enable it for this view:
-    dynamicNavbar: true
-});
-
-// Handle Cordova Device Ready Event
-$$(document).on('deviceready', function() {
-    console.log("Device is ready!");
+    domCache: true //enable inline pages
 });
 
 
 // Now we need to run the code that will be executed only for About page.
-
-// Option 1. Using page callback for page (for "about" page in this case) (recommended way):
-myApp.onPageInit('about', function (page) {
-    // Do something here for "about" page
-
-})
-
-// Option 2. Using one 'pageInit' event handler for all pages:
-$$(document).on('pageInit', function (e) {
-    // Get page data from event data
-    var page = e.detail.page;
-
-    if (page.name === 'about') {
-        // Following code will be executed for page with data-page attribute equal to "about"
-        myApp.alert('Here comes About page');
-    }
-})
-
-// Option 2. Using live 'pageInit' event handlers for each page
-$$(document).on('pageInit', '.page[data-page="about"]', function (e) {
-    // Following code will be executed for page with data-page attribute equal to "about"
-    myApp.alert('Here comes About page');
-})
 $("input").focus(function () {
     //we add the css class blur to the elements that we would like to blur on focus
     $("img").addClass("blur");
@@ -49,4 +26,8 @@ $("input").focus(function () {
     //we remove the blur class which will remove the blur from the elements specified when we are no longer focused on an input
     $("img").removeClass("blur");
     $("p").removeClass("blur");
+    });
+$(".a").click(function () {
+    mainView.router.load({ pageName: 'cars' });
 });
+myApp.init();
