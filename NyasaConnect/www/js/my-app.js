de0d250be3df264607cd1c19ba9d6b55a740c50d
John-Paul Chirwa <johnpaulchirwa@live.co.uk>
2018-09-19 17:38:57 +0100

    19/09/2018 Commit

--- NyasaConnect/www/js/my-app.js
+++ NyasaConnect/www/js/my-app.js
@@ -1,30 +1,9 @@
-var isAndroid = Framework7.prototype.device.android === true;
-var isIos = Framework7.prototype.device.ios === true;
-
-// Set Template7 global devices flags
-Template7.global = {
-    android: isAndroid,
-    ios: isIos
-};
-
-// If we need to use custom DOM library, let's save it to $$ variable:
 var $$ = Dom7;
 
-if (isAndroid || isIos) {
-    $$('head').append(
-        '<link rel="stylesheet" href="lib/framework7/css/framework7.material.min.css">' +
-        '<link rel="stylesheet" href="lib/framework7/css/framework7.material.colors.min.css">' +
-        '<link rel="stylesheet" href="lib/framework7-icons/css/framework7-icons.css">' +
-        '<link rel="stylesheet" href="css/styles.css">'+
-        //'<link rel="stylesheet" href="lib/framework7/css/framework7.ios.min.css">'+
-        '<link rel="stylesheet" href="lib/framework7/css/framework7.ios.colors.min.css">'
-    );
-}
 // Initialize app
 var myApp = new Framework7({
-    // Enable Material theme for Android device only
-    material: isAndroid ? true : false,
-    // Enable Template7 pages
+    swipePanel: 'left',
+    preloadPreviousPage: true,
     template7Pages: true,
     pushState: true, //Android back button go back a page
     init: false //Disable App's automatic initialization
