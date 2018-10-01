17a1e2c57c9302f464b8350526e3ee774992a3f5
John-Paul Chirwa <johnpaulchirwa@live.co.uk>
2018-10-01 11:44:18 +0100

    Master Template

--- NyasaConnect/www/js/my-app.js
+++ NyasaConnect/www/js/my-app.js
@@ -21,7 +21,6 @@ myApp.onPageInit('index', function (page) {
 })
 $(".floater").click(function () {
     alert("ok")
-    navigator.camera.getPicture(uploadPhoto, onError, camearaOptions);
 });
 
 // Now we need to run the code that will be executed only for About page.
@@ -51,35 +50,3 @@ myApp.init();
 var swiper = app.swiper.get('.swiper-container');
 
 swiper.slideNext();
-
-var camearaOptions = {
-    quality: 100,
-    destinationType: navigator.camera.DestinationType.FILE_URI,
-    sourceType: navigator.camera.PictureSourceType.PHOTOLIBRARY
-}
-
-
-function onError(err) { alert(error); }
-
-function uploadPhoto(imageURI) {
-    var options = new FileUploadOptions();
-    options.fileKey = "file";
-    options.fileName = imageURI.substr(imageURI.lastIndexOf('/') + 1);
-    options.mimeType = "image/jpeg";
-
-    var params = new Object();
-    params.value1 = "test";
-    params.value2 = "param";
-
-    options.params = params;
-    options.chunkedMode = false;
-
-    var ft = new FileTransfer();
-    ft.upload(imageURI, "http://192.168.11.125/phonegap/upload/upload.php",
-        function (result) {
-            console.log(JSON.stringify(result));
-        },
-        function (error) {
-            console.log(JSON.stringify(error));
-        }, options);
-}
