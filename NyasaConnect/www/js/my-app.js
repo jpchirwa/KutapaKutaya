9a80a48995b2c5d8453cf40534c9509a7b9561fe
John-Paul Chirwa <johnpaulchirwa@live.co.uk>
2018-10-03 00:20:52 +0100

    Server working

--- NyasaConnect/www/js/my-app.js
+++ NyasaConnect/www/js/my-app.js
@@ -22,7 +22,7 @@ myApp.onPageInit('index', function (page) {
     });
 })
 $(".floater").click(function () {
-    cameraGetPicture();
+    getImage();
 });
 
 // Now we need to run the code that will be executed only for About page.
@@ -50,7 +50,7 @@ $(".d").click(function () {
 myApp.init();
 function cameraTakePicture() {
     navigator.camera.getPicture(onSuccess, onFail, {
-        quality: 50,
+        quality: 100,
         destinationType: Camera.DestinationType.DATA_URL
     });
 
@@ -80,6 +80,41 @@ function cameraGetPicture() {
     }
 
 }
+
+var camearaOptions = {
+    quality: 100,
+    destinationType: navigator.camera.DestinationType.FILE_URI,
+    sourceType: navigator.camera.PictureSourceType.PHOTOLIBRARY
+}
+function getImage() {
+    navigator.camera.getPicture(uploadPhoto, onError, camearaOptions);
+}
+
+function onError(err) { alert(error); }
+
+function uploadPhoto(imageURI) {
+    var options = new FileUploadOptions();
+    options.fileKey = "file";
+    options.fileName = imageURI.substr(imageURI.lastIndexOf('/') + 1);
+    options.mimeType = "image/jpeg";
+
+    var params = new Object();
+    params.value1 = "test";
+    params.value2 = "param";
+
+    options.params = params;
+    options.chunkedMode = false;
+
+    var ft = new FileTransfer();
+    ft.upload(imageURI, "http://192.168.1.103/phonegap/upload/upload.php",
+        function (result) {
+            console.log(JSON.stringify(result));
+        },
+        function (error) {
+            console.log(JSON.stringify(error));
+        }, options);
+}
+
 var swiper = app.swiper.get('.swiper-container');
 
 swiper.slideNext();
